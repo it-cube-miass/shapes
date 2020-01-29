a83899d9322766b381c5f50c7bdc986bd13cbe3e
haschish <haschish@gmail.com>
2020-01-29 21:57:03 +0500

    changed test findMaxAreaShape

--- test/ShapeTest.java
+++ test/ShapeTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -28,6 +29,13 @@ class ShapeTest {
 
     @Test
     void getColor() {
+        Method m = null;
+        try {
+            m = clazz.getMethod("getColor");
+            assertEquals("class java.lang.String", m.getReturnType().toString());
+        } catch (NoSuchMethodException e) {
+            fail("method findMaxAreaShape not found");
+        }
         assertEquals(color, shape.getColor());
     }
 
@@ -45,27 +53,27 @@ class ShapeTest {
     @Test
     void findMaxAreaShape() {
         try {
-            Method m = clazz.getMethod("findMaxAreaShape");
+            Method method = clazz.getMethod("findMaxAreaShape", Shape[].class);
+            Class returnValue = method.getReturnType();
+            assertEquals("Shape", returnValue.getName(), "incorrect return type");
         } catch (NoSuchMethodException e) {
-            fail("method findMaxAreaShape not found");
+            fail("method findMaxAreaShape(Shape[] arr) not found");
         }
 
-        Shape s1 = new Shape("green"){
+        class T extends Shape {
+            double value;
+            T(double value) {
+                super("black");
+                this.value = value;
+            }
             @Override
             public double getArea() {
-                return 2;
+                return value;
             }
-        };
-
-        Shape s2 = new Shape("green"){
-            @Override
-            public double getArea() {
-                return 2;
-            }
-        };
-        Shape[] arr = {s1, s2};
-
-        Shape max = Shape.findMaxAreaShape(arr);
-        assertEquals(s2, max);
+        }
+        Shape s1 = new T(4);
+        Shape[] arr = {new T(1), new T(2), s1, new T(3)};
+        Shape maxAreaShape = Shape.findMaxAreaShape(arr);
+        assertEquals(s1, maxAreaShape, "method works incorrected");
     }
 }
